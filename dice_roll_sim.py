a1d5fb2fc94b05d0f5742689638e136937393d99
saradonin <m.jemieljanczuk@gmail.com>
2023-03-29 00:36:00 +0200

    final roll function added

--- dice_roll_sim.py
+++ dice_roll_sim.py
@@ -1,16 +1,6 @@
 import random
 
 
-def dice_roll(size=6):
-    if size in [3, 4, 6, 8, 10, 12, 20, 100]:
-        return random.randint(1, size)
-    else:
-        return f"{size}-sided dice doesn't exist."
-
-def multiple_dices(number):
-  return [dice_roll() for i in range(number)]
-
-
 def get_player_input():
     print("Enter a dice you'd like to roll: ")
     input_str = input()
@@ -29,13 +19,33 @@ def get_player_input():
     return number, dice_size, modifier
 
 
+def dice_roll(size=6):
+    if size in [3, 4, 6, 8, 10, 12, 20, 100]:
+        return random.randint(1, size)
+    else:
+        return f"{size}-sided dice doesn't exist."
+
+
+def multiple_dices(number):
+    return [dice_roll() for i in range(number)]
+
+
+def roll():  # work in progress
+    number, dice_size, modifier = get_player_input()
+
+    rolls = [dice_roll(dice_size) for i in range(number+1)]
+
+    return number, dice_size, modifier, rolls
+
+
 # test
 
 # for i in range(0,10):
 #   print(dice_roll(6))
 
-
 # for i in range(0,10):
 #   print(multiple_dices(4))
 
-print(get_player_input())
+# print(get_player_input())
+
+print(roll())
