ecc6144151a7fe7d4fc434b55e04d80754491bf2
saradonin <m.jemieljanczuk@gmail.com>
2023-03-29 00:21:46 +0200

    get user input

--- dice_roll_sim.py
+++ dice_roll_sim.py
@@ -11,10 +11,31 @@ def multiple_dices(number):
   return [dice_roll() for i in range(number)]
 
 
+def get_player_input():
+    print("Enter a dice you'd like to roll: ")
+    input_str = input()
+
+    number = int(input_str.split('d')[0])
+
+    if "+" in input_str:
+        modifier = int(input_str.split('+')[-1])
+    elif "-" in input_str:
+        modifier = - int(input_str.split('-')[-1])
+    else:
+        modifier = 0
+
+    dice_size = int(input_str.replace("d", " ").replace("+", " ").replace("-", " ").split(' ')[1])
+
+    return number, dice_size, modifier
+
+
 # test
+
 # for i in range(0,10):
 #   print(dice_roll(6))
 
 
-for i in range(0,10):
-  print(multiple_dices(4))
+# for i in range(0,10):
+#   print(multiple_dices(4))
+
+print(get_player_input())
