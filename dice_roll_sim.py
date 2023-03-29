55d3d2b36cbc73576a587f09cb79f9616b7b09bd
saradonin <m.jemieljanczuk@gmail.com>
2023-03-29 16:39:52 +0200

    Unnecessary items removed. Uppercase D test replaced by lower(). Test for empty string added. Test for empty number of dices added.

--- dice_roll_sim.py
+++ dice_roll_sim.py
@@ -3,49 +3,69 @@ import random
 
 def get_player_input():
     print("Enter a dice you'd like to roll: ")
-    input_str = input()
-    number = 1
-    if 'd' in input_str:
-        number = int(input_str.split('d')[0])
-    elif 'D' in input_str:
-        number = int(input_str.split('D')[0])
+    while True:
+        input_str = input().lower()
 
-    if "+" in input_str:
-        modifier = int(input_str.split('+')[-1])
-    elif "-" in input_str:
-        modifier = - int(input_str.split('-')[-1])
-    else:
-        modifier = 0
+        # test for empty input
+        if not input_str:
+            print("Invalid input. Try again.")
+            continue
 
-    dice_size = int(input_str.replace("d", " ").replace("D", " ").replace("+", " ").replace("-", " ").split(' ')[1])
+        # test for 'd'
+        if 'd' not in input_str:
+            print("Invalid input. Try again.")
+            continue
 
-    return number, dice_size, modifier
+        try:
+            # number of dice to roll
+            number = input_str.split('d')[0]
+            if number == '':
+                number = 1
+            else:
+                number = int(number)
+
+            # modifier
+            if "+" in input_str:
+                modifier = int(input_str.split('+')[-1])
+            elif "-" in input_str:
+                modifier = - int(input_str.split('-')[-1])
+            else:
+                modifier = 0
+
+            # dice size
+            dice_size = int(input_str.replace('d', ' ').replace('+', ' ').replace('-', ' ').split(' ')[1])
+            if dice_size in [3, 4, 6, 8, 10, 12, 20, 100]:
+                return number, dice_size, modifier
+            else:
+                print("Invalid dice size. Try again.")
+
+        except ValueError:
+            print("Invalid input. Try again.")
 
 
-def dice_roll(size=6):
-    if size in [3, 4, 6, 8, 10, 12, 20, 100]:
-        return random.randint(1, size)
-    else:
-        return f"{size}-sided dice doesn't exist."
+def dice_roll(dice_size=6):
+    return random.randint(1, dice_size)
 
 
 def roll():  # work in progress
-
     number, dice_size, modifier = get_player_input()
 
     rolls = [dice_roll(dice_size) for i in range(number)]
     result = sum(rolls) + modifier
 
-    return number, dice_size, modifier, rolls, result
+    return f"""
+Number of dices: {number} 
+Dice size: {dice_size} 
+Modifier: {modifier} 
+Rolls: {rolls}
+Result: {result}
+"""
 
 
 # test
 
 # for i in range(0,10):
 #   print(dice_roll(6))
-
-# for i in range(0,10):
-#   print(multiple_dices(4))
 
 # print(get_player_input())
 
