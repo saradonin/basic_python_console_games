9cb7100f38e77ccc6ec335024b4b19e0629345ca
saradonin <m.jemieljanczuk@gmail.com>
2023-03-29 17:10:43 +0200

    Docstrings added

--- dice_roll_sim.py
+++ dice_roll_sim.py
@@ -2,6 +2,15 @@ import random
 
 
 def get_player_input():
+    """
+    Prompts used to input string data defining parameters of dice rolls.
+    Takes string in format: xDy+z
+    where:
+    x - number of dices (optional, default = 1)
+    y - size of the single dice from set [3, 4, 6, 8, 10, 12, 20, 100]
+    z - modifier (optional, default = 0)
+    :return: tuple of int values (number, dice_size, modifier)
+    """
     print("Enter a dice you'd like to roll: ")
     while True:
         input_str = input().lower()
@@ -44,22 +53,34 @@ def get_player_input():
 
 
 def dice_roll(dice_size=6):
+    """
+    Takes dice size as input (default value = 6), returns the result of the random roll.
+    :param dice_size: int - size of the dice
+    :return: int - result of a single dice roll
+    """
     return random.randint(1, dice_size)
 
 
 def roll():
+    """
+    Calls external function get_player_input to generate input data: number (int) - number of dices, dice_size (int) - size of the dice, modifier (int) - result modifier.
+    Returns the sum of all dice rolls modified by modifier value.
+    :return: int - sum of all rolls modified by modifier value
+    """
     number, dice_size, modifier = get_player_input()
 
-    rolls = [dice_roll(dice_size) for i in range(number)]
+    rolls = ([dice_roll(dice_size) for i in range(number)])
     result = sum(rolls) + modifier
+    return result
 
-    return f"""
-Number of dices: {number} 
-Dice size: {dice_size} 
-Modifier: {modifier} 
-Rolls: {rolls}
-Result: {result}
-"""
+
+# # for detailed information about the dice rolls uncomment 6 lines below
+#     return f"""Number of dices: {number}
+# Dice size: {dice_size}
+# Modifier: {modifier}
+# Rolls: {rolls_str}
+# Result: {result}
+# """
 
 
 print(roll())
